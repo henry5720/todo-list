b33d19194efaa5915923f2f80c2be91d246c43ed
henry5720 <henry5720@hotmail.com.tw>
2022-04-27 16:46:34 +0800

    [fix] 修改清單刪除功能

--- src/pages/List/components/Item.js
+++ src/pages/List/components/Item.js
@@ -4,30 +4,34 @@ import React, { useState } from "react";
 const Item=function({listData, del}){
     return (
         <ul className="item">
-            <Lis del={del} listData={listData}/>
+            {listData.map(item=>{
+                const {id, note}=item;
+                return (
+                    <Lis key={id} del={del} id={id} note={note}/>
+                );
+            })}
         </ul>
     );
 }
 
-const Lis=function({listData, del}){
+const Lis=function({del, id, note}){
     function delNote(){
-        del(
-            prev=>{
-                console.log("prev:",prev);
-                return []
-            }
-        ); // setData()
+        // del={setData()}
+        del(prev=>{ 
+            const newPrev=prev.filter(item=>{
+                console.log("item.id:",item.id);
+                console.log("id:",id);
+                return item.id != id;
+            })
+            // console.log("newPrev:",newPrev);
+            return newPrev
+        }); 
     }
-    return listData.map(
-        (item)=>{
-            const {id, note}=item;
-            return (
-                <li key={id}>
-                    {note}
-                    <button onClick={delNote} className="remove">del</button>
-                </li>
-            );
-        }
+    return (
+        <li id={id}>
+            {note}
+            <button onClick={delNote} className="remove">del</button>
+        </li>
     );
 }
 
